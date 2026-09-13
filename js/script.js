f411eac5157a9f657a6c627783755f70c26ed551
RGoH-064 <1828000966@qq.com>
2026-09-13 19:14:57 +0800

    Site updated: 2026-09-13 19:14:56

--- js/script.js
+++ js/script.js
@@ -136,3 +136,14 @@
     $container.removeClass('mobile-nav-on');
   });
 })(jQuery);
+// KaTeX 自动渲染
+document.addEventListener('DOMContentLoaded', function() {
+  if (typeof renderMathInElement === 'function') {
+    renderMathInElement(document.body, {
+      delimiters: [
+        {left: '$$', right: '$$', display: true},
+        {left: '$', right: '$', display: false}
+      ]
+    });
+  }
+});
